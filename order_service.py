67917b7144bf7b47839066e268e78683a27a01f6
Jakob Rietzler <jakob.rietzler@gmail.com>
2026-04-16 19:45:38 +0200

    add logging for order service

--- order_service.py
+++ order_service.py
@@ -15,8 +15,11 @@ class Order(BaseModel):
 @app.post("/order")
 async def create_order(order: Order):
     # Create an HTTP client to communicate with the kitchen service
+    logger.info(f"Receive order with flavour {order.flavor}")
+    logger.debug(f"Order-json: {order}")
     async with httpx.AsyncClient() as client:
         try:
+            logger.info(f"Sending order with flavour {order.flavor}")
             # Send the order to the kitchen service
             response = await client.post(
                 "http://localhost:8001/prepare",
@@ -24,10 +27,13 @@ async def create_order(order: Order):
             )
             # Raise an error if the kitchen returned an error status code
             response.raise_for_status()
+            logger.info(f"Completed order with flavour {order.flavor}")
             return {"status": "completed", "kitchen_response": response.json()}
         except httpx.HTTPStatusError as e:
             # Kitchen returned an error (e.g., 503 if all cooks are busy)
+            logger.error(e.response.textlogger.error(f"Kitchen error - Status: {e.response.status_code}, Response: {e.response.text}"))
             raise HTTPException(status_code=e.response.status_code, detail="Kitchen failed to process order")
-        except httpx.RequestError:
+        except httpx.RequestError as e:
+            logger.error(f"Request error - Kitchen service is unavailable:")
             # Could not connect to the kitchen service
             raise HTTPException(status_code=503, detail="Kitchen unavailable")
